a8bf77dad67159471007baa5c87dd20825eefee8
hhoo1645 <43479514+hhoo1645@users.noreply.github.com>
2018-12-17 15:07:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -296,6 +296,29 @@ client.user.setGame(`مولع خيو`,"http://twitch.tv/S-F")
 client.user.setStatus("online")
 });
 
+client.on('message', message => {
+   
+    let args = message.content.split(' ').slice(1).join(' ');
+   
+  if (message.content === 'ping') {
+      message.channel.send(`<@${message.author.id}> Ping..!`)
+  }
+ 
+ 
+  if (message.content.startsWith('$bc')) {
+          if (!args[0]) {
+message.channel.send("**$bc <message>**");
+return;
+}
+message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+   m.send(`${args}`);
+ 
+});
+  }
+ 
+});
+
 
 
 
